e5c66435ecfc49e6c33cb1402be50b47d64f456d
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-11-03 14:42:00 -0600

    HomeScreen Terminado y reformado

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -9,13 +9,38 @@ import TitleC from "../components/TitleC";
 import ButtonC from "../components/ButtonC";
 function HomeScreen(props) {
   return (
-      <ScreenC>
-        <CIrclesD></CIrclesD>
-      </ScreenC>
-
+    <ScreenC style={styles.a}>
+      <CIrclesD></CIrclesD>
+      <View style={styles.titleContainer}>
+        <TitleC title="Bienvenido a SquareCo" />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <ButtonC title="Crear cuenta" color="naranja"></ButtonC>
+        <View style={styles.separacion} />
+        <ButtonC title="Iniciar sesión" color="azul"></ButtonC>
+      </View>
+    </ScreenC>
   );
 }
 const styles = StyleSheet.create({
+  a: {
+    alignItems: "center",
+  },
+  titleContainer: {
+    width: "100%",
+    marginTop: 40,
+    paddingHorizontal: 40,
+  },
+  buttonsContainer: {
+    flex: 1,
+    marginTop: -120,
+    justifyContent: "center",
+    width: "70%",
+    paddingHorizontal: 30,
+  },
+  separacion: {
+    padding: 20,
+  },
 });
 export default HomeScreen;
 {/* <View style={styles.buttonsContainer}>
