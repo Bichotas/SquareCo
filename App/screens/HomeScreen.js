a6b4563114afaf303baa02b8bd206c6c081c9fa1
depresion <depresion@pop-os.localdomain>
2021-11-03 14:22:32 -0600

    Se cambiaron el diseño de los circulos a undocumento propio
    Lo que se hizo es que se importo como componete, esto para que no se viera sucio el codigo de de la pantalla de inicio

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -2,66 +2,20 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import ScreenC from "../components/ScreenC";
 
-
+// DIseños
+import CIrclesD from "../designs/CIrclesD";
 // Import Components
 import TitleC from "../components/TitleC";
 import ButtonC from "../components/ButtonC";
 function HomeScreen(props) {
   return (
-    <ScreenC>
-      {/* Circulos pendejos */}
-      <View style={styles.headerGraphic}>
-        <View style={styles.leftCircle} />
-      </View>
-      <View style={styles.footerGraph}>
-        <View style={styles.bottomCircle} />
-        <View style={styles.bottomCircle2} />
-      </View>
-    </ScreenC>
+      <ScreenC>
+        <CIrclesD></CIrclesD>
+      </ScreenC>
+
   );
 }
 const styles = StyleSheet.create({
-  headerGraphic: {
-    position: "absolute",
-    width: "100%",
-    top: -50,
-    zIndex: -100,
-  },
-  leftCircle: {
-    backgroundColor: "#6A4BD8",
-    position: "absolute",
-    borderRadius: 200,
-    width: 400,
-    height: 400,
-    top: -100,
-    left: -160,
-    opacity: 0.5,
-  },
-  footerGraph: {
-    position: "absolute",
-    width: "100%",
-    bottom: -100,
-  },
-  bottomCircle: {
-    backgroundColor: "#3CA49A",
-    position: "absolute",
-    borderRadius: 100,
-    width: 200,
-    height: 200,
-    bottom: 10,
-    right: -80,
-    opacity: 0.5,
-  },
-  bottomCircle2: {
-    backgroundColor: "#3CA49A",
-    position: "absolute",
-    borderRadius: 100,
-    width: 200,
-    height: 200,
-    bottom: -30,
-    right: 10,
-    opacity: 0.5,
-  },
 });
 export default HomeScreen;
 {/* <View style={styles.buttonsContainer}>
