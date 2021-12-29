d5de3a7fd3eb4193de70e8d09fe2f953aca1d777
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-12-29 20:48:17 +0000

    Pantalla de Configuracion
    Lo que se hizo fue usar un "FlatList' y se renderizaba un objeto el cual la manera en la que se renderizaba era con un Boton.
    Falta cambiar la propiedad de cuando se presione ya que este cambia a un color azul que no es de la paleta de colores. Se debe de usar la propiedad _pressed

--- App/screens/SettingsScreen.js
+++ App/screens/SettingsScreen.js
@@ -1,20 +1,56 @@
 import React from "react";
-import { NativeBaseProvider, Center, Button } from "native-base";
+import {
+  NativeBaseProvider,
+  Center,
+  Button,
+  FlatList,
+  Container,
+  Box,
+  Text,
+} from "native-base";
 
+const confOptions = [
+  { item: 1, name: "Datos de la cuenta", go: "DatosCuenta" },
+  { item: 2, name: "Datos de envio", go: "DatosEnvio" },
+  { item: 3, name: "Cambiar contraseña", go: "CambiarContraseña" },
+];
 export default function SettingsScreen() {
   return (
     <NativeBaseProvider>
-      <Center flex={1}>
-        <Button
-          padding={4}
-          _pressed={{
-            bg: "yellow.600",
-          }}
-          borderRadius="2xl"
-        >
+      <Box background={"purple.600"} h={12} justifyContent={"center"}>
+        <Text padding={2} color={"white"} fontSize={20} fontWeight={"bold"}>
           Configuracion
-        </Button>
-      </Center>
+        </Text>
+      </Box>
+      <Box p={3}>
+        <FlatList
+          data={confOptions}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Button
+              bg={"#D1D1D1"}
+              width={"100%"}
+              padding={5}
+              marginY={5}
+              borderColor={"black"}
+              borderWidth={3}
+              justifyContent={"flex-start"}
+              fontWeight={"extrabold"}
+              borderRadius={20}
+              _text={{
+                fontWeight: "bold",
+                fontSize: "lg",
+              }}
+              shadow={(10, 5, 3, 5)}
+              onPress={() => {
+                console.log(item.go);
+              }}
+            >
+              {item.name}
+            </Button>
+          )}
+        />
+      </Box>
     </NativeBaseProvider>
   );
 }
