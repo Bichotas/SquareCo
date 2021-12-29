6c6a514da79a7932b9948afb566ddbf25dcfb6fe
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-12-29 23:24:47 +0000

    Configuracion de Cuenta - Falta Agregar Detalles - Moti, libreria parecida a FramerMOtion
    Falta cambiar los botones y tambien colocar el segment Tab de tipo de cuenta, este debemos de ver cual es parecido al que hay en framer

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import RegisterScreen from "./App/screens/RegisterScreen";
 import SettingsScreen from "./App/screens/SettingsScreen";
 
 // Actual
-import ProfileStore from "./App/screens/ProfileStore";
+import AccountSettingsScreen from "./App/screens/AccountSettingsScreen";
 
 import colors from "./App/config/colors";
 const Drawer = createDrawerNavigator();
@@ -39,7 +39,7 @@ const DrawerNavigator = () => (
     <Drawer.Screen name="Carrito" component={RegisterScreen} />
     <Drawer.Screen name="Tiendas" component={LoginScreen} />
     <Drawer.Screen name="Configuracion" component={SettingsScreen} />
-    <Drawer.Screen name="Actual" component={ProfileStore} />
+    <Drawer.Screen name="Actual" component={AccountSettingsScreen} />
   </Drawer.Navigator>
 );
 
