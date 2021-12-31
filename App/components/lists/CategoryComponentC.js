568c35b8189e2d2d0d66ae9852261fb0e87d3621
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2021-12-30 18:08:49 -0600

    Falta colocar la opcion del icono
    Memorias de plastico
    
    Co-Authored-By: IRISALINA <89416903+IRISALINA@users.noreply.github.com>

--- App/components/lists/CategoryComponentC.js
+++ App/components/lists/CategoryComponentC.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Box, Center, Pressable, Text, View } from "native-base";
+
+// Falta colocar la opcion de icono adentro del recuadro
+
 function CategoryComponentC({ name, onPress, backgroundColor = "gray.400" }) {
   return (
     <Center marginBottom={5} padding={4}>
@@ -11,7 +14,11 @@ function CategoryComponentC({ name, onPress, backgroundColor = "gray.400" }) {
         _pressed={{
           bg: "gray.600",
         }}
-      ></Pressable>
+      >
+        
+        {/* Aqui debera poner el icono que debe */}
+
+      </Pressable>
       <View width={"80%"}>
         <Text fontWeight={"bold"} fontSize={[16, 22, 30]} textAlign={"center"}>
           {name}
