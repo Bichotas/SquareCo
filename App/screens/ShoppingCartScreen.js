df198291a65e5f8312595b9e02de1ce22514cef0
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2021-12-30 19:25:04 -0600

    Prototipo para toda la pantalla
    Arreglar con sus propiedades y luego hacer cada una de las cosas componentes, esto para que el codigo de la pantalla sea mas limpio
    
    Co-Authored-By: IRISALINA <89416903+IRISALINA@users.noreply.github.com>

--- App/screens/ShoppingCartScreen.js
+++ App/screens/ShoppingCartScreen.js
@@ -4,6 +4,7 @@ import {
   Button,
   Center,
   Container,
+  Divider,
   HStack,
   NativeBaseProvider,
   ScrollView,
@@ -20,7 +21,7 @@ function ShoppingCartScreen(props) {
       <HeaderScreenC title={"Carrito"} />
 
       <Center justifyContent={"flex-start"} flex={1}>
-        <ScrollView>
+        <ScrollView padding={(4, 4, 4)} marginBottom={3}>
           {/* <VStack space={5}>
             <Box width={340} height={90} bg={"gray.400"} borderRadius={15}>
               <HStack space={2}>
@@ -35,7 +36,227 @@ function ShoppingCartScreen(props) {
             ></Box>
           </VStack> */}
           {/* Este es para el componente del RPODUCTO */}
-          <VStack>
+          <VStack space={4}>
+            <HStack
+              space={3}
+              bg={"gray.800"}
+              height={100}
+              width={350}
+              borderRadius={12}
+            >
+              <Box
+                bg={"gray.500"}
+                // El tamaño correcto deberia de ser 70, pero el boton de eliminar se ve mal por el espacio a su derecha
+                size={65}
+                alignSelf={"center"}
+                marginLeft={4}
+                borderRadius={10}
+              />
+              <VStack justifyContent={"space-between"}>
+                <Container width={168} height={"100%"}>
+                  <Text
+                    color={"white"}
+                    marginTop={3}
+                    fontSize={16}
+                    fontWeight={"bold"}
+                    numberOfLines={2}
+                  >
+                    [Nombre del productoadsfadsfs]
+                  </Text>
+
+                  <Text color={"white"} marginBottom={4} fontSize={15} italic>
+                    Precio
+                  </Text>
+                </Container>
+              </VStack>
+              <Center justifyContent={"flex-end"}>
+                <Button
+                  borderRadius={7}
+                  py={1.2}
+                  px={2}
+                  marginBottom={3}
+                  marginRight={3}
+                >
+                  Eliminar
+                </Button>
+              </Center>
+            </HStack>
+            <HStack
+              space={3}
+              bg={"gray.800"}
+              height={100}
+              width={350}
+              borderRadius={12}
+            >
+              <Box
+                bg={"gray.500"}
+                // El tamaño correcto deberia de ser 70, pero el boton de eliminar se ve mal por el espacio a su derecha
+                size={65}
+                alignSelf={"center"}
+                marginLeft={4}
+                borderRadius={10}
+              />
+              <VStack justifyContent={"space-between"}>
+                <Container width={168} height={"100%"}>
+                  <Text
+                    color={"white"}
+                    marginTop={3}
+                    fontSize={16}
+                    fontWeight={"bold"}
+                    numberOfLines={2}
+                  >
+                    [Nombre del productoadsfadsfs]
+                  </Text>
+
+                  <Text color={"white"} marginBottom={4} fontSize={15} italic>
+                    Precio
+                  </Text>
+                </Container>
+              </VStack>
+              <Center justifyContent={"flex-end"}>
+                <Button
+                  borderRadius={7}
+                  py={1.2}
+                  px={2}
+                  marginBottom={3}
+                  marginRight={3}
+                >
+                  Eliminar
+                </Button>
+              </Center>
+            </HStack>
+            <HStack
+              space={3}
+              bg={"gray.800"}
+              height={100}
+              width={350}
+              borderRadius={12}
+            >
+              <Box
+                bg={"gray.500"}
+                // El tamaño correcto deberia de ser 70, pero el boton de eliminar se ve mal por el espacio a su derecha
+                size={65}
+                alignSelf={"center"}
+                marginLeft={4}
+                borderRadius={10}
+              />
+              <VStack justifyContent={"space-between"}>
+                <Container width={168} height={"100%"}>
+                  <Text
+                    color={"white"}
+                    marginTop={3}
+                    fontSize={16}
+                    fontWeight={"bold"}
+                    numberOfLines={2}
+                  >
+                    [Nombre del productoadsfadsfs]
+                  </Text>
+
+                  <Text color={"white"} marginBottom={4} fontSize={15} italic>
+                    Precio
+                  </Text>
+                </Container>
+              </VStack>
+              <Center justifyContent={"flex-end"}>
+                <Button
+                  borderRadius={7}
+                  py={1.2}
+                  px={2}
+                  marginBottom={3}
+                  marginRight={3}
+                >
+                  Eliminar
+                </Button>
+              </Center>
+            </HStack>
+            <HStack
+              space={3}
+              bg={"gray.800"}
+              height={100}
+              width={350}
+              borderRadius={12}
+            >
+              <Box
+                bg={"gray.500"}
+                // El tamaño correcto deberia de ser 70, pero el boton de eliminar se ve mal por el espacio a su derecha
+                size={65}
+                alignSelf={"center"}
+                marginLeft={4}
+                borderRadius={10}
+              />
+              <VStack justifyContent={"space-between"}>
+                <Container width={168} height={"100%"}>
+                  <Text
+                    color={"white"}
+                    marginTop={3}
+                    fontSize={16}
+                    fontWeight={"bold"}
+                    numberOfLines={2}
+                  >
+                    [Nombre del productoadsfadsfs]
+                  </Text>
+
+                  <Text color={"white"} marginBottom={4} fontSize={15} italic>
+                    Precio
+                  </Text>
+                </Container>
+              </VStack>
+              <Center justifyContent={"flex-end"}>
+                <Button
+                  borderRadius={7}
+                  py={1.2}
+                  px={2}
+                  marginBottom={3}
+                  marginRight={3}
+                >
+                  Eliminar
+                </Button>
+              </Center>
+            </HStack>
+            <HStack
+              space={3}
+              bg={"gray.800"}
+              height={100}
+              width={350}
+              borderRadius={12}
+            >
+              <Box
+                bg={"gray.500"}
+                // El tamaño correcto deberia de ser 70, pero el boton de eliminar se ve mal por el espacio a su derecha
+                size={65}
+                alignSelf={"center"}
+                marginLeft={4}
+                borderRadius={10}
+              />
+              <VStack justifyContent={"space-between"}>
+                <Container width={168} height={"100%"}>
+                  <Text
+                    color={"white"}
+                    marginTop={3}
+                    fontSize={16}
+                    fontWeight={"bold"}
+                    numberOfLines={2}
+                  >
+                    [Nombre del productoadsfadsfs]
+                  </Text>
+
+                  <Text color={"white"} marginBottom={4} fontSize={15} italic>
+                    Precio
+                  </Text>
+                </Container>
+              </VStack>
+              <Center justifyContent={"flex-end"}>
+                <Button
+                  borderRadius={7}
+                  py={1.2}
+                  px={2}
+                  marginBottom={3}
+                  marginRight={3}
+                >
+                  Eliminar
+                </Button>
+              </Center>
+            </HStack>
             <HStack
               space={3}
               bg={"gray.800"}
@@ -82,6 +303,10 @@ function ShoppingCartScreen(props) {
             </HStack>
           </VStack>
         </ScrollView>
+        <Divider height={2} bg={"black"}></Divider>
+        <Text>HOLA</Text>
+        <Text>HOLA</Text>
+        <Text>HOLA</Text>
       </Center>
     </NativeBaseProvider>
   );
