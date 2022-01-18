59a5f858c4d2c63d897984f2096122f0024b0266
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2022-01-18 14:09:10 -0600

    Se registrar la referencia y se guarda el documento
    Esto guarda una clave-valor llamada prueba
    Tener cuidado con setDoc d ela linea 42

--- App/screens/ConfigScreens/ShippingDataScreen.js
+++ App/screens/ConfigScreens/ShippingDataScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import {
   Box,
   Text,
@@ -24,10 +24,6 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 // Cositas de firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const storage = getStorage(app);
 const validationSchema = Yup.object().shape({
   streetName: Yup.string().required().label("Dirección"),
   postalCode: Yup.number().required().max(5).label("Código postal"),
@@ -35,6 +31,18 @@ const validationSchema = Yup.object().shape({
 });
 import ReturnArrow from "../../components/ReturnArrow";
 function ShippingDataScreen({ navigation }) {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const firestore = getFirestore(app);
+  const storage = getStorage(app);
+  const profileContext = useContext(ProfileContext);
+  const { uid } = profileContext.profile;
+
+  const docRef = doc(firestore, "users", `${uid}`, "shippingData", `${uid}`);
+  setDoc(docRef, {
+    prueba: "prueba",
+  });
+
   const pressHandler = () => {
     console.log("Pressing");
     navigation.goBack();
@@ -43,66 +51,69 @@ function ShippingDataScreen({ navigation }) {
     <NativeBaseProvider>
       <ScrollView>
         <ReturnArrow onPress={pressHandler} />
-        <KeyboardAvoidingView>
-          <Box flex={1} padding={6}>
-            <Text
-              fontSize={"4xl"}
-              fontWeight={"bold"}
-              color={"black"}
-              textAlign={"center"}
-              padding={2}
-            >
-              Datos de envio
-            </Text>
-            <Divider bg={"black"} h={1} />
-            <Form
-              initialValues={{
-                streetName: "",
-                postalCode: "",
-                phoneNumber: "",
-              }}
-              onSubmit={(values) => console.log(values)}
-            >
-              <VStack space={2} mt="0">
-                <Text fontWeight={"bold"} fontSize={16} padding={2}>
-                  Nombre de la calle
+        <ProfileContext.Consumer>
+          {({ profile, setProfile }) => (
+            <KeyboardAvoidingView>
+              <Box flex={1} padding={6}>
+                <Text
+                  fontSize={"4xl"}
+                  fontWeight={"bold"}
+                  color={"black"}
+                  textAlign={"center"}
+                  padding={2}
+                >
+                  Datos de envio
                 </Text>
-                <FormField
-                  name={"streetName"}
-                  placeholder={"Dirección de envío"}
-                />
-                <HStack space={5}>
-                  <VStack>
+                <Divider bg={"black"} h={1} />
+                <Form
+                  initialValues={{
+                    streetName: "",
+                    postalCode: "",
+                    phoneNumber: "",
+                  }}
+                  onSubmit={(values) => console.log(values)}
+                >
+                  <VStack space={2} mt="0">
                     <Text fontWeight={"bold"} fontSize={16} padding={2}>
-                      Nombre de la cuenta
+                      Nombre de la calle
                     </Text>
                     <FormField
-                      name={"postalCode"}
-                      placeholder={"Código postal"}
-                      keyboardType="numeric"
-                      maxLength={5}
+                      name={"streetName"}
+                      placeholder={"Dirección de envío"}
                     />
+                    <HStack space={5}>
+                      <VStack>
+                        <Text fontWeight={"bold"} fontSize={16} padding={2}>
+                          Nombre de la cuenta
+                        </Text>
+                        <FormField
+                          name={"postalCode"}
+                          placeholder={"Código postal"}
+                          keyboardType="numeric"
+                          maxLength={5}
+                        />
+                      </VStack>
+                      <VStack>
+                        <Text fontWeight={"bold"} fontSize={16} padding={2}>
+                          Número telefonico
+                        </Text>
+                        <FormField
+                          name={"phoneNumber"}
+                          keyboardType="numeric"
+                          maxLength={10}
+                          placeholder={"Número telefonico"}
+                        />
+                      </VStack>
+                    </HStack>
                   </VStack>
-                  <VStack>
-                    <Text fontWeight={"bold"} fontSize={16} padding={2}>
-                      Número telefonico
-                    </Text>
-                    <FormField
-                      name={"phoneNumber"}
-                      keyboardType="numeric"
-                      maxLength={10}
-                      placeholder={"Número telefonico"}
-                    />
-                  </VStack>
-                </HStack>
-              </VStack>
-              <Center>
-                <SubmitButton title={"Guardar cambios"} />
-              </Center>
-            </Form>
-            {/* En vez de un switch - Se debe de usar un Segment Component o un Tab View */}
-          </Box>
-        </KeyboardAvoidingView>
+                  <Center>
+                    <SubmitButton title={"Guardar cambios"} />
+                  </Center>
+                </Form>
+              </Box>
+            </KeyboardAvoidingView>
+          )}
+        </ProfileContext.Consumer>
       </ScrollView>
     </NativeBaseProvider>
   );
