74da02bf27ba88bb3256f6a748348272fe4c888f
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-02-09 14:31:19 +0000

    Cambios en configuracion de base de datos

--- App/database/firebaseConfig.js
+++ App/database/firebaseConfig.js
@@ -4,13 +4,5 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCDwCAcymiXKT-E2E_Xtb_msp8UKFKs6hw",
-  authDomain: "pruebaauth-57328.firebaseapp.com",
-  projectId: "pruebaauth-57328",
-  storageBucket: "pruebaauth-57328.appspot.com",
-  messagingSenderId: "755106899236",
-  appId: "1:755106899236:web:cb5c7ddb88810c1045ffb1",
-};
 
 export default firebaseConfig;
