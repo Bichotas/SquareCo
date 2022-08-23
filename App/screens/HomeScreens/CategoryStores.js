f73826ccd653d45487998fbc67c67005509092f8
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2022-08-23 17:24:28 -0500

    pendejadas

--- App/screens/HomeScreens/CategoryStores.js
+++ App/screens/HomeScreens/CategoryStores.js
@@ -1,15 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Text,
+  Center,
+  Container,
+  NativeBaseProvider,
+  FlatList,
+} from "native-base";
 
 export default function CategoryStores({ route }) {
+  const [storeCategory, setstoreCategory] = useState([]);
   useEffect(() => {
     console.log(route.params);
   }, []);
 
   return (
-    <View>
-      <Text>{route.params.label}</Text>
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Box
+          width={"60%"}
+          marginTop={5}
+          borderRadius={10}
+          borderWidth={1}
+          borderColor={route.params.backgroundColor}
+          paddingY={2}
+          background={route.params.backgroundColor}
+          justifyContent="center"
+          alignItems={"center"}
+        >
+          <Text
+            fontSize={"lg"}
+            fontWeight={"bold"}
+            letterSpacing={"1"}
+            color={"gray.200"}
+          >
+            {route.params.label}
+          </Text>
+        </Box>
+        <FlatList />
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
