a04b5ac191dedde65429f23dfd62a34e3edb1f44
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2022-08-24 20:12:10 -0500

    Pendehadas

--- App/screens/HomeScreens/CategoryStores.js
+++ App/screens/HomeScreens/CategoryStores.js
@@ -8,47 +8,8 @@ import {
   NativeBaseProvider,
   FlatList,
 } from "native-base";
-import ReturnArrow from "../../components/ReturnArrow";
-import { CommonActions } from "@react-navigation/native";
-import { ScrollView } from "native-base";
-import {
-  collection,
-  onSnapshot,
-  orderBy,
-  query,
-  where,
-} from "firebase/firestore";
-import { db } from "../../utils/db.server";
-export default function CategoryStores({ route, navigation }) {
-  const [storeCategory, setstoreCategory] = useState([]);
-  // useEffect(() => {
-  //   let category = route.params.category;
-  //   let collectionRef = collection(db, "stores");
-  //   const q = query(
-  //     collectionRef,
-  //     // Faltaría obtener el id de la tienda
-  //     where("category", "==", route.params.where)
-  //   );
 
-  //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  //     setstoreCategory(
-  //       querySnapshot.docs.map((doc) => ({
-  //         id: doc.id,
-  //         title: doc.data().title,
-  //         price: doc.data().price,
-  //         description: doc.data().description,
-  //         createdAt: doc.data().createdAt,
-  //         category: doc.data().category,
-  //         storeProfileId: doc.data().storeProfileId,
-  //       }))
-  //     );
-  //   });
-  //   console.log(storeCategory);
-  //   return unsubscribe;
-  // }, []);
-  useEffect(() => {
-    let condition = route.params.navigate;
-  }, []);
+export default function CategoryStores({ route, navigation }) {
   return (
     <NativeBaseProvider>
       <Center flex={1}>
@@ -77,5 +38,3 @@ export default function CategoryStores({ route, navigation }) {
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({});
